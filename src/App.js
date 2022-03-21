8a724b4086c9d8dc1094c7c5c4482f5dabd0f39d
Ciciovan Bogdan <ciciovan.bogdan@gmail.com>
2022-03-21 16:09:54 +0200

    bug fix

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import Recipe from './Recipe';
 import './App.css';
 
 const App = () => {
-  const APP_ID = 'e8c46754';
-  const APP_KEY = '536a5d6ba56598b56d40c959dde5b5a2';
+  const APP_ID = '09632557';
+  const APP_KEY = '8bd4b85258c22175edbc67396676aed0';
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState('');
   const [query, setQuery] = useState('chicken');
-
-  useEffect(() => {
-    getRecipes();
-  }, [query])
 
   const getRecipes = async () => {
     const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
   }
+
+  useEffect(() => {
+    getRecipes()
+  }, [query])
 
   const updateSearch = ({ target }) => {
     setSearch(target.value);
--- src/Recipe.js
+++ src/Recipe.js
@@ -11,7 +11,7 @@ const Recipe = ({ title, calories, image, ingredients }) => {
                 ))}
             </ol>
             <p>{calories}</p>
-            <img className={style.image} src={image} alt='image' />
+            <img className={style.image} src={image} alt='food' />
         </div>
     );
 }
